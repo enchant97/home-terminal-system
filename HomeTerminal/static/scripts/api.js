ce621ecdd51237ec0e590b3555005967e890bb0f
enchant97 <enchant97@users.noreply.github.com>
2020-02-25 22:38:37 +0000

    first api xhr addition

--- HomeTerminal/static/scripts/api.js
+++ HomeTerminal/static/scripts/api.js
@@ -27,3 +27,44 @@ function httpGetAsyncJSON(theUrl, callback, ...callback_args) {
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
     xmlHttp.send(null);
 }
+
+function httpPOSTAsyncJSON(){
+    //TODO: implement
+}
+
+function refresh_notifications_count(){
+    // loads a new count of notifications
+    // into notification icon
+    //TODO: implement later after 2.0
+}
+
+function new_messages(messages){
+    // adds new messages to the messages box on dashboard
+    const mesage_table = document.getElementById("loadedmessages");
+    mesage_table.innerHTML = "";//TODO: remove when the lastupdated date is implemented
+    for (const message of messages.loaded_data){
+        const row = document.createElement("tr");
+        const col1 = document.createElement("td");
+        const col2 = document.createElement("td");
+        const closebnt = document.createElement("button");
+
+        row.setAttribute("id", "message_" + message.id);
+        col1.innerText = message.message;
+        closebnt.setAttribute("onclick", `delete_message(${message.id})`)
+        closebnt.innerText = "Close";
+
+        col2.append(closebnt);
+        row.append(col1);
+        row.append(col2);
+
+        mesage_table.append(row);
+    }
+}
+
+function delete_message(mess_id){
+    //TODO: implement with xhr
+}
+
+function do_messages_refresh(){
+    httpGetAsyncJSON("/api/messages", new_messages);
+}
