523d2ab8837e05c3d314361ba62e167f619ecded
enchant97 <enchant97@users.noreply.github.com>
2020-02-26 18:46:15 +0000

    added rememberme

--- HomeTerminal/views/main.py
+++ HomeTerminal/views/main.py
@@ -2,33 +2,34 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from ..dao import try_login_user
-from ..models import User, db
+from ..models import User
 
 main = Blueprint("main", __name__)
 
 @main.route("/", methods=["POST", "GET"])
 def index():
     if current_user.is_authenticated:
-            return redirect(url_for("home.dashboard"))
+        return redirect(url_for("home.dashboard"))
     #TODO: remove User.query.all()
     return render_template("main/index.html", users=User.query.all())
 
 @main.route("/login", methods=["POST"])
 def do_login():
     if current_user.is_authenticated:
-            return redirect(url_for("home.dashboard"))
-
+        return redirect(url_for("home.dashboard"))
     username = request.form.get("username")
     password = request.form.get("password")
+    remember_me = request.form.get("rememberme", False)
     if not username or not password:
         flash("You have left the form blank", "warning")
     user = try_login_user(username, password)
     if user:
-        #TODO: change to allow remember me to be chosen on the login form
-        login_user(user, remember=True)
+        if remember_me:
+            login_user(user, remember=True)
+        else:
+            login_user(user, remember=False)
         return redirect(url_for("home.dashboard"))
-    else:
-        flash("username or password not correct", "warning")
+    flash("username or password not correct", "warning")
     return redirect(url_for(".index"))
 
 @main.route("/logout")
