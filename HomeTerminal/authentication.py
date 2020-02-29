c63cd7f3df962d0cda76360ddc09007009cc99b9
enchant97 <enchant97@users.noreply.github.com>
2020-02-29 16:02:42 +0000

    v2.0.2 made redirect to login page

--- HomeTerminal/authentication.py
+++ HomeTerminal/authentication.py
@@ -1,7 +1,9 @@
-from .models import User
 from flask_login import LoginManager
 
+from .models import User
+
 login_manager = LoginManager()
+login_manager.login_view = 'main.index'
 
 @login_manager.user_loader
 def user_loader(user_id):
