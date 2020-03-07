3782ff20030eb307e2bb8ea9258573c4c9341197
enchant97 <enchant97@users.noreply.github.com>
2020-03-07 17:07:41 +0000

    fix pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,8 @@ from os import environ
 import pytest
 
 from HomeTerminal import create_app, db
-from HomeTerminal.dao import new_account
-from HomeTerminal.models import User
+from HomeTerminal.database.dao.user import new_account
+from HomeTerminal.database.models.user import User
 from HomeTerminal.utils import hash_str
 
 
