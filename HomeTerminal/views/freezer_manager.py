d12acc0f836dac9c6b250d1c83d637df2b4cd93d
Leo Spratt <enchant97@users.noreply.github.com>
2020-05-01 21:49:26 +0100

    remove the need for arguments in fm url

--- HomeTerminal/views/freezer_manager.py
+++ HomeTerminal/views/freezer_manager.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, flash, render_template, request
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required
 
 from ..database.dao.exceptions import RowDoesNotExist
@@ -29,35 +29,36 @@ def report_expiring():
     items = get_fm4_expiring()
     return render_template("/freezer_manager/report_expiring.html", items=items)
 
-@fm.route("/edit", methods=["GET", "POST"])
+@fm.route("/edit/", defaults={"item_id": None}, methods=["GET", "POST"])
+@fm.route("/edit/<int:item_id>", methods=["GET", "POST"])
 @login_required
-def edit():
+def edit(item_id):
     if request.method == "POST":
         try:
-            id_ = request.form.get("id", None)
             name = request.form["name"]
             amount = request.form["amount"]
             category = request.form["category"].capitalize()
             removed = bool(request.form.get("removed", 0))
             expire = None
-            if not id_:
+            if not item_id:
                 if request.form.get("custom_expire", False) == "1":
                     expire = datetime.strptime(request.form["expire_date"], "%Y-%m-%d")
                 else:
                     expire = calc_expire_date(request.form["expire"])
-            edit_fm4_item(name, category, amount, expire, removed, id_)
+            edit_fm4_item(name, category, amount, expire, removed, item_id)
             flash("Added Entry")
+            # return back to adding a new entry so the user cant edit the same item again
+            return redirect(url_for(".edit"))
         except KeyError:
             flash("Missing required fields!", "warning")
         except RowDoesNotExist:
             flash("An item with that id does not exist!", "error")
     categories = get_fm4_categories()
-    edit_item_id = request.args.get("item_id", default="", type=str)
-    if edit_item_id == "":
+    if not item_id:
         default_item = FM_Item(name="", expire_date="", category_id="", quantity=0, id_=None)
     else:
         try:
-            default_item = get_fm4_item(edit_item_id)
+            default_item = get_fm4_item(item_id)
         except RowDoesNotExist:
             flash("An item with that id does not exist!", "error")
     return render_template(
