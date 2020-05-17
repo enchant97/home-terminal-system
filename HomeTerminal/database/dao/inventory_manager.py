cd2080ce8eb54d103b0daf0af4924502f5143a2e
Leo Spratt <enchant97@users.noreply.github.com>
2020-05-17 17:38:42 +0100

    ability to edit im rows

--- HomeTerminal/database/dao/inventory_manager.py
+++ HomeTerminal/database/dao/inventory_manager.py
@@ -63,6 +63,62 @@ def new_item(name: str, box_id: int, quantity: int = 1, type_id: int = None, in_
     db.session.commit()
     return new_item_row
 
+def edit_location(loc_id, **kwargs):
+    """
+    used to edit a location row
+    args:
+        loc_id : location id for what value to update
+        **kwargs : values to update
+    """
+    location_row = Location.query.filter_by(id_=loc_id)
+    if not location_row.count():
+        raise RowDoesNotExist(f"location row id '{loc_id}' does not exist")
+    location_row.update(kwargs)
+    db.session.commit()
+    return location_row
+
+def edit_type(type_id, **kwargs):
+    """
+    used to edit a type row
+    args:
+        type_id : type id for what value to update
+        **kwargs : values to update
+    """
+    type_row = Type.query.filter_by(id_=type_id)
+    if not type_row.count():
+        raise RowDoesNotExist(f"type row id '{type_id}' does not exist")
+    type_row.update(kwargs)
+    db.session.commit()
+    return type_row
+
+def edit_box(box_id, **kwargs):
+    """
+    used to edit a box row
+    args:
+        box_id : box id for what value to update
+        **kwargs : values to update
+    """
+    box_row = Box.query.filter_by(id_=box_id)
+    if not box_row.count():
+        raise RowDoesNotExist(f"box row id '{box_id}' does not exist")
+    box_row.update(kwargs)
+    db.session.commit()
+    return box_row
+
+def edit_item(item_id, **kwargs):
+    """
+    used to edit a item row
+    args:
+        item_id : item id for what value to update
+        **kwargs : values to update
+    """
+    item_row = Item.query.filter_by(id_=item_id)
+    if not item_row.count():
+        raise RowDoesNotExist(f"item row id '{item_id}' does not exist")
+    item_row.update(kwargs)
+    db.session.commit()
+    return item_row
+
 def get_type(removed: bool = False, first=False, **filters):
     """
     returns Type rows
--- HomeTerminal/views/inventory_manager.py
+++ HomeTerminal/views/inventory_manager.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, flash, render_template, request
 
+from ..database.dao.exceptions import RowDoesNotExist
 from ..database.dao import inventory_manager as im_dao
 
 im = Blueprint("im", __name__)
@@ -25,76 +26,91 @@ def view():
 @im.route("/edit-box/", defaults={"box_id": None}, methods=["GET", "POST"])
 @im.route("/edit-box/<box_id>", methods=["GET", "POST"])
 def edit_box(box_id):
-    if request.method == "POST":
-        name = request.form["box-name"]
-        loc_id = request.form["box-loc"]
+    try:
+        if request.method == "POST":
+            name = request.form["box-name"].lower()
+            loc_id = request.form["box-loc"]
+            if box_id:
+                im_dao.edit_box(box_id, name=name, loc_id=loc_id)
+                flash("updated!")
+            else:
+                im_dao.new_box(loc_id, name)
+                flash("added new box!")
+        locs = im_dao.get_locations()
         if box_id:
-            #TODO: implement the edit of box
-            flash("this is not implemented yet", "error")
-        else:
-            im_dao.new_box(loc_id, name)
-            flash("added new box!")
-    locs = im_dao.get_locations()
-    if box_id:
-        return render_template(
-            "inventory_manager/edit-box.html",
-            locations=locs, box=im_dao.get_box(first=True, id_=box_id))
+            return render_template(
+                "inventory_manager/edit-box.html",
+                locations=locs, box=im_dao.get_box(first=True, id_=box_id))
+    except RowDoesNotExist:
+        flash("box row does not exist", "error")
+        locs = im_dao.get_locations()
     return render_template("inventory_manager/edit-box.html", locations=locs)
 
 @im.route("/edit-item/", defaults={"item_id": None}, methods=["GET", "POST"])
 @im.route("/edit-item/<item_id>", methods=["GET", "POST"])
 def edit_item(item_id):
-    if request.method == "POST":
-        name = request.form["item-name"]
-        box_id = request.form["item-box"]
-        type_id = request.form["item-type"]
-        quantity = request.form["item-quantity"]
-        in_box = request.form.get("item-inbox", False, bool)
+    try:
+        if request.method == "POST":
+            name = request.form["item-name"].lower()
+            box_id = request.form["item-box"]
+            type_id = request.form["item-type"]
+            quantity = request.form["item-quantity"]
+            in_box = request.form.get("item-inbox", False, bool)
+            if item_id:
+                im_dao.edit_item(
+                    item_id, name=name, box_id=box_id,
+                    type_id=type_id, quantity=quantity, in_box=in_box)
+                flash("updated!")
+            else:
+                im_dao.new_item(name, box_id, quantity, type_id, in_box)
+                flash("added new item!")
+        boxes = im_dao.get_box()
+        types = im_dao.get_type()
         if item_id:
-            #TODO: implement the edit of item
-            flash("this is not implemented yet", "error")
-        else:
-            im_dao.new_item(name, box_id, quantity, type_id, in_box)
-            flash("added new item!")
-    boxes = im_dao.get_box()
-    types = im_dao.get_type()
-    if item_id:
-        return render_template(
-            "inventory_manager/edit-item.html", types=types,
-            boxes=boxes, item=im_dao.get_item(first=True, id_=item_id))
+            return render_template(
+                "inventory_manager/edit-item.html", types=types,
+                boxes=boxes, item=im_dao.get_item(first=True, id_=item_id))
+    except RowDoesNotExist:
+        flash("item row does not exist", "error")
     return render_template("inventory_manager/edit-item.html", types=types, boxes=boxes)
 
 @im.route("/edit-type/", defaults={"type_id": None}, methods=["GET", "POST"])
 @im.route("/edit-type/<type_id>", methods=["GET", "POST"])
 def edit_type(type_id):
-    if request.method == "POST":
-        name = request.form["type-name"]
+    try:
+        if request.method == "POST":
+            name = request.form["type-name"].lower()
+            if type_id:
+                im_dao.edit_type(type_id, name=name)
+                flash("updated!")
+            else:
+                im_dao.new_type(name)
+                flash("added new item type!")
         if type_id:
-            #TODO: implement the edit of item
-            flash("this is not implemented yet", "error")
-        else:
-            im_dao.new_type(name)
-            flash("added new item type!")
-    if type_id:
-        return render_template(
-            "inventory_manager/edit-type.html",
-            item_type=im_dao.get_type(first=True, id_=type_id))
+            return render_template(
+                "inventory_manager/edit-type.html",
+                item_type=im_dao.get_type(first=True, id_=type_id))
+    except RowDoesNotExist:
+        flash("type row does not exist", "error")
     return render_template("inventory_manager/edit-type.html")
 
 @im.route("/edit-location/", defaults={"location_id": None}, methods=["GET", "POST"])
 @im.route("/edit-location/<location_id>", methods=["GET", "POST"])
 def edit_location(location_id):
-    if request.method == "POST":
-        name = request.form["loc-name"]
-        comment = request.form.get("loc-comment", None)
+    try:
+        if request.method == "POST":
+            name = request.form["loc-name"].lower()
+            comment = request.form.get("loc-comment", None)
+            if location_id:
+                im_dao.edit_location(location_id, name=name, comment=comment)
+                flash("updated!")
+            else:
+                im_dao.new_location(name, comment)
+                flash("added new location!")
         if location_id:
-            #TODO: implement the edit of location
-            flash("this is not implemented yet", "error")
-        else:
-            im_dao.new_location(name, comment)
-            flash("added new location!")
-    if location_id:
-        return render_template(
-            "inventory_manager/edit-location.html",
-            location=im_dao.get_locations(first=True, id_=location_id))
+            return render_template(
+                "inventory_manager/edit-location.html",
+                location=im_dao.get_locations(first=True, id_=location_id))
+    except RowDoesNotExist:
+        flash("location row does not exist", "error")
     return render_template("inventory_manager/edit-location.html")
