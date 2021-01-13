3df68869ad761baefb5206b3499160aa29e236ec
Leo Spratt <enchant97@users.noreply.github.com>
2021-01-13 20:17:10 +0000

    use __main__ instead of run.py

--- HomeTerminal/__main__.py
+++ HomeTerminal/__main__.py
@@ -4,5 +4,11 @@ For running the HTS
 from . import create_app
 
 if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
     app = create_app()
-    app.run(host="localhost", port=5050, debug=True)
+    server = pywsgi.WSGIServer(
+        ('localhost', 5050), app,
+        handler_class=WebSocketHandler,
+        log=app.logger)
+    server.serve_forever()
--- run.py
+++
@@ -1,8 +0,0 @@
-from HomeTerminal import create_app
-
-if __name__ == "__main__":
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    app = create_app()
-    server = pywsgi.WSGIServer(('localhost', 5050), app, handler_class=WebSocketHandler, log=app.logger)
-    server.serve_forever()
