78a4a38c325d3555c70f8484bc745b2e922a63da
Leo Spratt <enchant97@users.noreply.github.com>
2021-06-17 11:44:40 +0100

    add new remove/restore for reminders app

--- HomeTerminal/views/reminder.py
+++ HomeTerminal/views/reminder.py
@@ -1,11 +1,8 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required
 
+from ..database import dao
 from ..database.dao.exceptions import RowDoesNotExist
-from ..database.dao.reminder import (get_reminder_tasks, get_reminder_types,
-                                     get_reminders, new_reminder,
-                                     new_reminder_task, remove_reminder)
-from ..database.dao.user import get_users
 from ..helpers.calculations import html_date
 
 reminder = Blueprint("reminder", __name__)
@@ -20,16 +17,16 @@ def view():
             r_type_id = request.form.get("reminder-type-id", None, int)
             show_removed = request.form.get("show-removed", False, bool)
             if r_type_id:
-                loaded_reminders = get_reminders(
+                loaded_reminders = dao.reminder.get_reminders(
                     reminder_type_id=r_type_id,
                     removed=show_removed)
             else:
-                loaded_reminders = get_reminders(removed=show_removed)
+                loaded_reminders = dao.reminder.get_reminders(removed=show_removed)
         except KeyError:
             flash("Missing required form data", "error")
     return render_template(
         "reminder/view.html",
-        reminders=loaded_reminders, types=get_reminder_types(), r_type_id=r_type_id)
+        reminders=loaded_reminders, types=dao.reminder.get_reminder_types(), r_type_id=r_type_id)
 
 @reminder.route("/new", methods=["GET", "POST"])
 @login_required
@@ -43,17 +40,17 @@ def new():
             datedue = request.form.get("datedue", None, html_date)
             tasks = request.form.getlist("atask")
 
-            added_reminder = new_reminder(
+            added_reminder = dao.reminder.new_reminder(
                 content, r_type, is_priority,
                 datedue, user_for_id)
             if tasks:
-                new_reminder_task(added_reminder.id_, *tasks)
+                dao.reminder.new_reminder_task(added_reminder.id_, *tasks)
             flash("added entry!")
         except KeyError:
             flash("Missing required form data", "error")
     return render_template(
-        "reminder/new.html", users=get_users(),
-        reminder_types=get_reminder_types())
+        "reminder/new.html", users=dao.user.get_users(),
+        reminder_types=dao.reminder.get_reminder_types())
 
 @reminder.route("/edit/<int:reminder_id>", methods=["GET", "POST"])
 @login_required
@@ -62,16 +59,16 @@ def edit(reminder_id):
         removed = bool(request.form.get("removed", False))
         try:
             if removed:
-                remove_reminder(reminder_id)
+                dao.reminder.remove_reminder(reminder_id)
                 flash("Entry marked for removal")
             else:
-                remove_reminder(reminder_id, False)
+                dao.reminder.remove_reminder(reminder_id, False)
                 flash("Entry no longer marked for removal")
         except RowDoesNotExist:
             flash("reminder with that id does not exist!", "warning")
         return redirect(url_for(".view"))
-    loaded_reminder = get_reminders(only_first=True, id_=reminder_id)
-    loaded_tasks = get_reminder_tasks(reminder_id=reminder_id, removed=False)
+    loaded_reminder = dao.reminder.get_reminders(only_first=True, id_=reminder_id)
+    loaded_tasks = dao.reminder.get_reminder_tasks(reminder_id=reminder_id, removed=False)
     return render_template(
         "/reminder/edit.html",
         reminder=loaded_reminder, tasks=loaded_tasks)
@@ -82,7 +79,21 @@ def new_task(reminder_id):
     if request.method == "POST":
         tasks = request.form.getlist("atask")
         if tasks:
-            new_reminder_task(reminder_id, *tasks)
+            dao.reminder.new_reminder_task(reminder_id, *tasks)
         return redirect(url_for(".edit", reminder_id=reminder_id))
-    loaded_reminder = get_reminders(only_first=True, removed=False)
+    loaded_reminder = dao.reminder.get_reminders(only_first=True, removed=False)
     return render_template("reminder/new_tasks.html", reminder=loaded_reminder)
+
+@reminder.route("/remove/<int:item_id>")
+@login_required
+def remove_reminder(item_id: int):
+    dao.reminder.remove_reminder(item_id, True)
+    flash("removed reminder")
+    return redirect(url_for(".view"))
+
+@reminder.route("/restore/<int:item_id>")
+@login_required
+def restore_reminder(item_id: int):
+    dao.reminder.remove_reminder(item_id, False)
+    flash("restored reminder")
+    return redirect(url_for(".view"))
